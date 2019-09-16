6269c82fc984edc2ac30f9f18e19cf8308b21d9a
y-okudera <appledev.yuoku@gmail.com>
2019-09-16 14:33:17 +0900

    Initial Commit

---
+++ iOSSwiftUIPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSSwiftUIPractice
+//
+//  Created by Yuki Okudera on 2019/09/16.
+//  Copyright © 2019 Yuki Okudera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
